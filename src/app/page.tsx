3e5530df9fe40423b8ce343478e1b2dfce68f03c
Milind Mishra <milind.mishra4@gmail.com>
2024-05-18 13:13:23 +0530

    📦 NEW: add line tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "~/components/hero";
+import LineTabs from "~/components/linetabs";
 import { ProjectCarousel } from "~/components/project-carousel";
 
 export default function HomePage() {
   return (
     <>
       <Hero />
+      <LineTabs />
       <ProjectCarousel />
     </>
   );
