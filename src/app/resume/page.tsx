2a4afe46727276854a8e9a6757a4ec9257a16851
Milind Mishra <milind.mishra4@gmail.com>
2024-05-23 12:03:50 +0530

    🐛 FIX: resume mobile download btn

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,11 +5,20 @@ import AnimateChildren from "~/components/animate-children";
 export default function Resume() {
   return (
     <AnimateChildren>
-      <div className="flex flex-col items-center justify-center space-y-4">
+      <div className="hidden flex-col items-center justify-center sm:flex">
         <iframe
           src="https://docs.google.com/document/d/1ujYf9MIOEeH3UpaHKZmXm_9-W6XpSQ5LAcLUv0ue32k/preview?embedded=true"
           className="h-screen w-full rounded-md"
         />
+      </div>
+      <div className="flex flex-col items-center justify-center sm:hidden">
+        <a
+          href="/assets/resume/Milind_Kumar_Mishra_Resume_2024.pdf"
+          download
+          className="font-semibold text-blue-500 underline"
+        >
+          Download Resume (PDF) 📄
+        </a>
       </div>
     </AnimateChildren>
   );
